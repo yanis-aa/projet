c8ff79421db99f5481927f90af7a16086b3fcc7d
weel06 <60885609+weel06@users.noreply.github.com>
2020-03-04 12:16:29 +0100

    Add files via upload

---
+++ Pylone.java
@@ -0,0 +1,23 @@
+
+public class Pylone {
+	private String id;
+	private String ip;
+	private String port;
+	private String latitude;
+	private String longitude;
+	
+
+	public String getPort() {
+		return port;
+}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+		
+	}
+	public void setIp (String ip) {
+		this.ip = ip;
+	}
+}
