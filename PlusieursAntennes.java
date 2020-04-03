6758a867601e75fb3eb93dc7970ea872db53b7f3
mariamdeme <61144173+mariamdeme@users.noreply.github.com>
2020-04-03 20:40:11 +0200

    Update PlusieursAntennes.java

--- PlusieursAntennes.java
+++ PlusieursAntennes.java
@@ -1,4 +1,3 @@
-
 public class PlusieursAntennes {
 
 	
@@ -15,7 +14,7 @@ Antenne antenne2=new Antenne(-10, 15, 5050, -35, "horizontal", -60, -10 );
 Antenne antenne3=new Antenne(-5, 20, 3000, -25, "vertical", -18, -7 );
 //pour afficher pour chaque antenne ses caractéristiques et les antennes avec qui elle peut communiquer
 	
-System.out.printrln("voici l'antenne1 et ses caractéristiques sont:") ;
+System.out.println("voici l'antenne1 et ses caractéristiques sont:") ;
 	antenne1.affaiche();
 	//antenne1.joindre(antenne1, antenne2);
 if (antenne1.joindre(antenne2)== "true") {
@@ -25,7 +24,7 @@ if (antenne1.joindre(antenne2)== "true") {
 	System.out.println ("l'antenne1 peut joindre l'antenne3");
 }
 	
-System.out.printrln("voici l'antenne2 et ses caractéristiques sont:");
+System.out.println("voici l'antenne2 et ses caractéristiques sont:");
 	antenne2.affaiche();
 	//antenne2.joindre(antenne2, antenne1);
 if (antenne2.joindre(antenne2, antenne1)== "true") {
@@ -36,7 +35,7 @@ if (antenne2.joindre(antenne2, antenne1)== "true") {
 	
 }
 
-System.out.printrln("voici l'antenne3 et ses caractéristiques sont:");
+System.out.println("voici l'antenne3 et ses caractéristiques sont:");
 	antenne3.affaiche();
 	//antenne1.joindre(antenne1, antenne2);
 if (antenne3.joindre(antenne3, antenne1)== "true") {
@@ -46,11 +45,9 @@ if (antenne3.joindre(antenne3, antenne1)== "true") {
 	System.out.println ("l'antenne3 peut joindre l'antenne2");
 }
 
-/ *parce qu' on suppese  que une antenne peut-etre à la fois émettri
+/*parce qu' on suppose  que une antenne peut-etre à la fois émettri
 	et réceptrice et l'antenne1 peut joindre l'antenne2 sans 		
 	que cette drnière ne soit en mesure de la joindre en retour*/
 	
-
 }
-
 }
